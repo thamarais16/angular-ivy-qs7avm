943cb225ae81bff5d0ee1c20bf5a2a06e5bcbab9
thamarais16 <thamarais16@gmail.com>
2021-05-10 19:38:05 +0530

    2 files modified

--- src/app/pipes/pipes.component.ts
+++ src/app/pipes/pipes.component.ts
@@ -1,3 +1,4 @@
+import { error } from '@angular/compiler/src/util';
 import { Component, OnInit } from '@angular/core';
 import { Observable, pipe } from 'rxjs';
 import { shareReplay } from 'rxjs/operators';
@@ -9,36 +10,48 @@ import { shareReplay } from 'rxjs/operators';
 })
 export class PipesComponent implements OnInit {
   dates = new Date();
-  name: Observable<string>;
-  names: Observable<string>;
+
 
   constructor() { } 
 
   ngOnInit() {
     
-    this.names = new Observable(observer => {
-      setTimeout(() => {
-        observer.next("We are processing please wait");
-      }, 3000);
-
-      setTimeout(() => {
-        observer.next("Your name is Thamarai Selvan");
-      }, 7000);
-
-      observer.complete();
-    })
-    
-    this.names.subscribe(data => {
-      return data;
-    });
-
-    this.name = (this.names).pipe(shareReplay());
-    
 
   }
 
-   
+  public names: Observable<any> = new Observable(observer => {
+    setTimeout(() => {
+      observer.next("We are processing please wait");
+    }, 3000);
 
+    setTimeout(() => {
+      observer.next("Your name is Thamarai Selvan");
+    }, 7000);
+
+    setTimeout(() => {
+      observer.error("There is some error");
+    }, 10 * 1000);
+    
+    setTimeout(() => {
+      observer.complete();
+    }, 14 *1000);
+  }).pipe(shareReplay());
+
+  public nameList: Observable<any> = new Observable(observer => {
+
+
+    setTimeout(() => {
+      observer.next(["a", "b", "c"]);
+    }, 7000);
+  }).pipe(shareReplay());
+
+  
+  nameLis = new Promise(res => {
+      res("hi");
+    }).finally(()=>{
+      return 0;
+    })
+  
 
 
 
